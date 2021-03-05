d6a02065c956cafca0b6e885b275f1c500e42421
Nick <i@httpd.cc>
2021-03-05 16:19:09 +0800

    feat: 增加关闭路由功能

--- config/account.php
+++ config/account.php
@@ -10,14 +10,15 @@
 
 return [
     'route' => [
-        'prefix' => '',
+        'open' => env('USER_ACCOUNT_ROUTE_OPEN', true),
+        'prefix' => env('USER_ACCOUNT_ROUTE_PREFIX', ''),
     ],
     // 验证码相关
     'captcha' => [
         // 验证码位数
-        'number' => 6,
+        'number' => env('USER_ACCOUNT_CAPTCHA_NUM', 6),
         // 验证码有效期，单位s
-        'expired_time' => 600,
+        'expired_time' => env('USER_ACCOUNT_CAPTCHA_EXPIRED_TIME', 600),
     ],
 
     // 需要关闭的功能，详见：\Cblink\UserAccount\AccountConst::FEATURE
--- src/AccountServiceProvider.php
+++ src/AccountServiceProvider.php
@@ -30,21 +30,37 @@ class AccountServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $this->bootedEvent();
+        $this->bootRoutes();
+        $this->published();
+        $this->loadFileToMigrate();
+    }
+
+    public function bootedEvent()
+    {
         $this->app->booted(function ($app) {
             $account = app(UserActionEvent::class);
             event($account);
         });
+    }
 
-        Route::group(config('account.route', []), function () {
-            $routeFile = base_path('routes/account.php');
+    public function bootRoutes()
+    {
+        if (config('account.route.open', true)) {
+            Route::group(config('account.route', []), function () {
+                $routeFile = base_path('routes/account.php');
 
-            $loadFile = file_exists($routeFile) ?
-                $routeFile :
-                \dirname(__DIR__) . '/routers/account.php';
+                $loadFile = file_exists($routeFile) ?
+                    $routeFile :
+                    \dirname(__DIR__) . '/routers/account.php';
 
-            $this->loadRoutesFrom($loadFile);
-        });
+                $this->loadRoutesFrom($loadFile);
+            });
+        }
+    }
 
+    public function published()
+    {
         $this->publishes([
             \dirname(__DIR__) . '/migrations/' => database_path('migrations'),
         ], 'migrations');
@@ -52,7 +68,10 @@ class AccountServiceProvider extends ServiceProvider
         $this->publishes([
             \dirname(__DIR__) . '/config/account.php' => config_path('account.php'),
         ], 'config');
+    }
 
+    public function loadFileToMigrate()
+    {
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(\dirname(__DIR__) . '/migrations/');
         }
